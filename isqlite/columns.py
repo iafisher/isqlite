086938bebcbb86df39ff7499e9be7015f3e395da
Ian Fisher <iafisher@fastmail.com>
2021-05-25 08:30:12 -0400

    Delete deprecated column classes

--- isqlite/columns.py
+++ isqlite/columns.py
@@ -1,6 +1,3 @@
-from ._exception import ISQLiteError
-
-
 class BaseColumn:
     def __init__(self, name, *, required, extra=""):
         self.name = name
@@ -18,82 +15,6 @@ class BaseColumn:
         if self.extra:
             tokens.append(self.extra)
 
-        return tokens
-
-
-class Text(BaseColumn):
-    sql_type = "TEXT"
-
-    def _tokens(self):
-        tokens = [self.sql_type, "NOT NULL"]
-        if self.required:
-            tokens.append(f"CHECK({self.name} != '')")
-
-        if self.extra:
-            tokens.append(self.extra)
-
-        return tokens
-
-
-class Integer(BaseColumn):
-    sql_type = "INTEGER"
-
-    def __init__(self, *args, autoincrement=False, primary_key=False, **kwargs):
-        super().__init__(*args, **kwargs)
-        if autoincrement is True and primary_key is False:
-            raise ISQLiteError(
-                "`autoincrement` may only be True when `primary_key` is True."
-            )
-
-        self.autoincrement = autoincrement
-        self.primary_key = primary_key
-
-    def _tokens(self):
-        tokens = super()._tokens()
-        if self.primary_key:
-            tokens.append("PRIMARY KEY")
-
-        if self.autoincrement:
-            tokens.append("AUTOINCREMENT")
-
-        return tokens
-
-
-class Real(BaseColumn):
-    sql_type = "REAL"
-
-
-class Blob(BaseColumn):
-    sql_type = "BLOB"
-
-
-class Timestamp(BaseColumn):
-    sql_type = "TIMESTAMP"
-
-
-class Boolean(BaseColumn):
-    sql_type = "BOOLEAN"
-
-
-class Decimal(BaseColumn):
-    sql_type = "DECIMAL"
-
-
-class Time(BaseColumn):
-    sql_type = "TIME"
-
-
-class ForeignKey(BaseColumn):
-    sql_type = "INTEGER"
-
-    def __init__(self, name, other_table, **kwargs):
-        super().__init__(name, **kwargs)
-        self.other_table = other_table
-
-    def _tokens(self):
-        tokens = super()._tokens()
-        tokens.append("REFERENCES")
-        tokens.append(self.other_table)
         return tokens
 
 
