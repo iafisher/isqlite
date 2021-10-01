96891234150d46af5a551f0c7623b53916e19f36
Ian Fisher <iafisher@fastmail.com>
2021-10-01 07:11:53 -0400

    Re-export some names from sqliteparser

--- isqlite/__init__.py
+++ isqlite/__init__.py
@@ -2,6 +2,9 @@ import datetime
 import decimal
 import sqlite3
 
+# Re-export some names from sqliteparser.
+from sqliteparser.ast import OnConflict, OnDelete, OnUpdate
+
 from .core import AddColumnMigration, AlterColumnMigration
 from .core import BooleanColumnStub as BooleanColumn
 from .core import (
