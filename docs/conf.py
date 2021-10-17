c4ff8eb7663d9b8e4daa226926614ae1bd60342f
Ian Fisher <iafisher@fastmail.com>
2021-10-16 23:06:52 -0400

    More spacing in API docs

--- docs/conf.py
+++ docs/conf.py
@@ -46,8 +46,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_theme_options = {
+    'github_user': 'iafisher',
+    'github_repo': 'isqlite',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    '_static/custom.css',
+]
